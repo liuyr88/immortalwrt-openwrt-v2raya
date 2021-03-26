df2ffe04c276570d8e11c3b2fcf725ef3fd145da
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2021-03-26 22:07:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,39 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
-
-#vssr
-git clone https://github.com/jerrykuku/lua-maxminddb.git 
-git clone https://github.com/jerrykuku/luci-app-vssr.git 
-mv luci-app-vssr/ package/
-mv lua-maxminddb/ package/
-
-
-# Add luci-app-ssr-plus
-#pushd package/lean
-#git clone --depth=1 https://github.com/fw876/helloworld
-#popd
-
-# Clone community packages to package/community
-mkdir package/community
-pushd package/community
-
-# Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
-
-
-# Add luci-app-adguardhome
-#svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
-#svn co https://github.com/Lienol/openwrt/trunk/package/diy/adguardhome
-
-# Add luci-app-jd-dailybonus
-git clone --depth=1 https://github.com/jerrykuku/node-request
-git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
-
-
-# Add smartdns
-svn co https://github.com/pymumu/smartdns/trunk/package/openwrt ../smartdns
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/luci-app-smartdns ../luci-app-smartdns
-
-popd
-
