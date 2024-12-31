40b2ea85eb96763c643597204a92f40bfb9801af
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2024-12-31 08:36:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,10 +8,12 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# Modify default IP
+#Modify default IP 用户名  密码
 sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/Melt-OpenWrt/g' package/base-files/files/bin/config_generate
+sed -i 's/root:::0:99999:7:::/root:$1$m9enpRnu$TjbaRy7zgCDF.hXF6md7p1:20077:0:99999:7:::/g' package/base-files/files/etc/shadow
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.254/g' package/base-files/files/bin/config_generate
 #adguardhome
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 #mv luci-app-adguardhome/ package/
